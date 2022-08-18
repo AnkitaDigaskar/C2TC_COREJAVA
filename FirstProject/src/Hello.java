5ce1e7687477250091d96c47dc1d75462c4af810
Ankita <Ankita@LAPTOP-LKEUB33L>
2022-08-18 14:02:45 +0530

    first commit

---
+++ FirstProject/src/Hello.java
@@ -0,0 +1,115 @@
+
+public class Hello {
+	/*public static void main(String[] args) 
+	{
+	System.out.println("Hello World!");
+	}*/
+
+	/*public static void main(String[] args) {
+		
+		int value1 = 9/2;
+		float value2 = 10f/6f;
+		double value3 = 10d/6d;
+		System.out.println("value  1 = "+ value1);
+		System.out.println("value  2 = "+ value2);
+		System.out.println("value  3 = "+ value3);
+		
+	    int marker = 512;
+	    double percentage = marker * 0.46f;
+	    
+	    System.out.println("percentage:"+percentage);
+		
+	public static void main(String[] args) {
+		boolean var = false;
+		System.out.println(var);
+
+		char var1 = '\u00A7';
+		System.out.println(var1);
+		
+	public static void main(String[] args) {
+		
+		 int x = 10; int y = 11;
+		  
+		  if(!(x < y) || (x == y))
+		  { 
+			  System.out.println("Condition is TRUE");
+		  }
+		  else
+		  { 
+			  System.out.println("Condition is FALSE"); 
+		}
+		 
+
+		
+int ageOfBoy = 36;
+		int ageOfGirl = 25;
+
+		if ((ageOfBoy >= 21) && (ageOfGirl >= 18)) 
+		{
+			System.out.println("ready to get married!");
+		} 
+		else 
+		{
+			System.out.println("Wait for it kiddo!");
+		}
+
+
+	}
+
+}
+	public static void main(String[] args) {
+		int x = 5;
+
+		
+		 if (x == 5) 
+		  { 
+		  if (x >= 5) 
+		  {
+		  	System.out.println("X is greater than 5"); 
+		   }
+		  else 
+		  {
+		  System.out.println("X is smaller than 5"); 
+		  } 
+		 }
+		
+		if(x == 5)
+		{
+			System.out.println("X equals to 5");
+		}
+		else if(x > 5)
+		{
+			System.out.println("X is greater to 5");		
+		}
+		else 
+		{
+			System.out.println("X is smaller than 5");
+		}
+		
+	}
+}*/
+	public static void main(String[] args) 
+	{
+
+			for (int i = 1; i <= 5; i++)
+			{
+				for (int j = 1; j <= i; j++) 
+				{
+					System.out.print('@');
+				}
+				System.out.println();
+			}
+		}
+	}
+
+
+
+
+
+
+
+
+
+	
+	
+
