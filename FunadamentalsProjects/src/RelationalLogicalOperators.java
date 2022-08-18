2db257f8b8286f7ca83bc9dd866dd72574b01f00
Ankita <Ankita@LAPTOP-LKEUB33L>
2022-08-18 15:22:39 +0530

    first commit

---
+++ FunadamentalsProjects/src/RelationalLogicalOperators.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class RelationalLogicalOperators 
+{
+
+	public static void main(String[] args)
+	{
+		Random rand = new Random();
+		int i = rand.nextInt(100);
+		int j = rand.nextInt(100);
+		System.out.println("i="+i);
+		System.out.println("j="+j);
+		
+		System.out.println("i > j is " + (i > j));
+		
+		System.out.println("");
+	}
+}
