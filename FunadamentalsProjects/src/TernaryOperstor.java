46dbaed1f2e65f9deeba39c6b67b798a85b34d2c
Ankita <Ankita@LAPTOP-LKEUB33L>
2022-08-22 14:33:52 +0530

    first commit

---
+++ FunadamentalsProjects/src/TernaryOperstor.java
@@ -0,0 +1,13 @@
+
+public class TernaryOperstor {
+
+	public static void main(String[] args) 
+	{
+		int v = 1;
+		System.out.println(v == 1 ? "A" : "B");
+		v++;
+		System.out.println(v == 1 ? "A" : "B");
+
+	}
+
+}
