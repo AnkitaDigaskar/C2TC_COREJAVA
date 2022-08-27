1ba6e0ebf58b9006c079a3bc3452a49347a5ae31
Ankita <Ankita@LAPTOP-LKEUB33L>
2022-08-27 16:44:34 +0530

    first commit

---
+++ FiestWeek/src/com/cg/Week1/Main2.java
@@ -0,0 +1,16 @@
+package com.cg.Week1;
+
+public class Main2 {
+
+	public static void main(String[] args) 
+	{
+		//while(true);{
+			//System.out.println("Technoserve");}
+		
+		do {
+			System.out.println("Technoserve");
+		}while(false);
+
+	}
+
+}
