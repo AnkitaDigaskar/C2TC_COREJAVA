acc55197dc82a0d4a6d919e86051627c871cb02b
Ankita <Ankita@LAPTOP-LKEUB33L>
2022-08-27 16:07:31 +0530

    first commit

---
+++ FiestWeek/src/com/cg/Week1/Test1.java
@@ -0,0 +1,19 @@
+package com.cg.Week1;
+
+public class Test1 
+{
+	static void test1(float x)
+	{
+		System.out.println("float");
+	}
+	static void test1(double x)
+	{
+		System.out.println("double");
+	}
+	public static void main(String[] args) 
+	{
+		
+		test1(99.9);
+	}
+
+}
