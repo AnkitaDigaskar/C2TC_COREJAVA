8802febbbe6d0a69c377d1e26aabfd9b30dd68f7
Ankita <Ankita@LAPTOP-LKEUB33L>
2022-08-27 17:35:01 +0530

    first commit

---
+++ FiestWeek/src/com/cg/Week1/ContinueNestedForLoop.java
@@ -0,0 +1,23 @@
+package com.cg.Week1;
+
+public class ContinueNestedForLoop {
+
+	public static void main(String[] args) 
+	{
+
+		for(int i=1;i<=3;i++)
+		{
+			for(int j=1;j<=3;j++)
+			{
+				if(i==2&&j==2)
+				{
+					continue;
+					
+				}
+				System.out.println(i+" "+j);
+			}
+		}
+
+	}
+
+}
