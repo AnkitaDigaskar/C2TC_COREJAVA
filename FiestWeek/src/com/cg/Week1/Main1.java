3f850e7342e508628c55ddca21eb3a5d771db0bd
Ankita <Ankita@LAPTOP-LKEUB33L>
2022-08-27 16:38:08 +0530

    first commit

---
+++ FiestWeek/src/com/cg/Week1/Main1.java
@@ -0,0 +1,15 @@
+package com.cg.Week1;
+
+public class Main1 
+{
+
+	public static void main(String[] args)
+	{
+		String s="friends";
+		int x=1;
+		do {
+			System.out.println(s.charAt(x));
+		   }while(x<2);
+	}
+
+}
