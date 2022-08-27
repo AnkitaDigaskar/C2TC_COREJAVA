e8f4169c0d949945cd9dfd4667e4e104591ed5bb
Ankita <Ankita@LAPTOP-LKEUB33L>
2022-08-27 17:07:14 +0530

    first commit

---
+++ FiestWeek/src/com/cg/Week1/BreakWhileLoop.java
@@ -0,0 +1,21 @@
+package com.cg.Week1;
+
+public class BreakWhileLoop {
+
+	public static void main(String[] args)
+	{
+
+		int a=1;
+		while(a<=10)
+		{
+			if(a==5)
+			break;
+			System.out.println("\n\tStatement: " + a);
+			a++;
+			
+		}
+		System.out.println("\n\t End of program.");
+
+	}
+
+}
